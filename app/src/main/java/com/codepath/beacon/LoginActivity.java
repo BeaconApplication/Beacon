cf253ccb85fa1f29e9498a4e895fe1ce021316a8
ssaeed383 <74425182+ssaeed383@users.noreply.github.com>
2021-04-14 18:13:56 -0400

    Added Create Account

--- app/src/main/java/com/codepath/beacon/LoginActivity.java
+++ app/src/main/java/com/codepath/beacon/LoginActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.os.Handler;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnCreate = findViewById(R.id.btnCreate);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +46,22 @@ public class LoginActivity extends AppCompatActivity {
                 loginUser(username, password);
             }
         });
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick create account button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                createUser(username, password);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        loginUser(username, password);
+                    }
+                }, 750);   //0.5 seconds
+                //loginUser(username, password);
+            }
+        });
     }
 
     private void loginUser(String username, String password) {
@@ -50,12 +71,12 @@ public class LoginActivity extends AppCompatActivity {
             public void done(ParseUser user, ParseException e) {
                 if (e != null) {
                     Log.e(TAG, "Issue with login", e);
-                    Toast.makeText(LoginActivity.this, "Issue with login", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Issue with login - Please Check Connection and Credentials", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 goMainActivity();
-                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -63,5 +84,26 @@ public class LoginActivity extends AppCompatActivity {
     private void goMainActivity() {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+    }
+    public void createUser(String username, String password) {
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        //user.setEmail("email@example.com"); //App does not require email address when signing up.
+
+        // Other fields can be set just like any other ParseObject,
+        // using the "put" method, like this: user.put("attribute", "its value");
+        // If this field does not exists, it will be automatically created
+
+        user.signUpInBackground(e -> {
+            if (e == null) {
+                // Hooray! Let them use the app now.
+            } else {
+                // Sign up didn't succeed. Look at the ParseException
+                // to figure out what went wrong
+                Log.e(TAG, "Issue with login for account: " + username, e);
+                Toast.makeText(this, "Error Creating Account - Please Check Connection and Credentials", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
